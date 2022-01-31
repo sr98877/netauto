12963b8acd79c68121856ea12b9b5898cd5e48ea
sr98877 <srronak11@gmail.com>
2022-01-31 20:10:05 +0530

    Final commit for 3 commits
    new commit
    
    new version of all prod
    
    new sh
    
    new f3

--- f3.py
+++ f3.py
@@ -1,1 +1,15 @@
+<<<<<<< Updated upstream
+import psutil
+
+def check_cpu_usage(usge):
+    us = psutil.cpu_percent()
+    return us
+
+if check_cpu_usage(15):
+    print("more")
+else:
+    print("okay")
+=======
+print("hello people")
 print("hii everyone")
+>>>>>>> Stashed changes
---
+++ f4.sh
@@ -0,0 +1,2 @@
+a=$(`cat /proc/cpuinfo`)
+echo $a
